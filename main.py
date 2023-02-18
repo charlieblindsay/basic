b1caf35ba145e1b268dea65cf73f0c64a6a6ae18
Charlie Lindsay <charlieblindsay@gmail.com>
2023-02-18 19:45:16 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.title('Neural Style Transfer Demo')
+st.write('After the user has uploaded a content image and a style image, an image with the \'content\' of the content image and the \'style\' of style image is generated.')
+
+st.subheader('File upload')
